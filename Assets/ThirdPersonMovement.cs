7f5dc9b7404aae55ce147f4989f1fd8db65eb7fc
coderx <36203292+blcx@users.noreply.github.com>
2020-12-12 19:15:59 +0530

    working prototype

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        if (direction.magnitude >= 0.1f) //if there is input
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //get mouse look angle
+
+            //smooth the angle 
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //moving forward direction calculation
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir * speed * Time.deltaTime);  // move input    
+        
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
